3341a584ac457b48c881e6ee9beb5078d68748a8
VishwaTej26 <codelearnearn15lpa@gmail.com>
2025-06-15 14:47:27 +0530

    Update Integer-and-Float-Division-Result.py

--- Integer-and-Float-Division-Result.py
+++ Integer-and-Float-Division-Result.py
@@ -1,5 +1,5 @@
 #Will update this as I keep improving my python knowledge. Thanks for reading.
-let a = int(input("Enter a="))
-let b = int(input("Enter b="))
+leta = int(input("Enter a="))
+letb = int(input("Enter b="))
 print(leta//letb)
 print(leta/letb)
